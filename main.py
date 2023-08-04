eaab930a1b11cc1a61364a04a70d8f230a6d5020
mell3w <melbustazz@yandex.ru>
2023-08-04 20:28:33 +0300

    Создал функцию для обновления списка радиобатанов VPN, при изменении списка VPN

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ notebook.add(bwd_frame, text='Бевард')
 notebook.add(vpn_frame, text='VPN')
 
 
-# Create a Treeview Frame
+# Create a bwd frame
 
 tree_frame = Frame(bwd_frame)
 tree_frame.pack(pady=10)
@@ -54,117 +54,6 @@ tree_frame.pack(pady=10)
 
 tree_scroll = Scrollbar(tree_frame)
 tree_scroll.pack(side=RIGHT,fill=Y)
-
-
-
-# Create frame for vpn
-
-vpn_list_frame = Frame(vpn_frame)
-vpn_list_frame.pack(pady=10)
-tree_vpn_scroll = Scrollbar(vpn_list_frame)
-tree_vpn_scroll.pack(side=RIGHT, fill=Y)
-tree_vpn = ttk.Treeview(vpn_list_frame, yscrollcommand = tree_vpn_scroll,
-                        selectmode='extended',
-                        columns=('ID','owner','name','login','password'),
-                        height=10, show='headings')
-tree_vpn.column('ID', width=30, anchor=CENTER)
-tree_vpn.column('owner', width=250, anchor=CENTER)
-tree_vpn.column('name',width=250, anchor=CENTER)
-tree_vpn.column('login', width=250,anchor=CENTER)
-tree_vpn.column('password',width=250, anchor=CENTER)
-tree_vpn.heading('ID', text='ID')
-tree_vpn.heading('owner', text='Владелец')
-tree_vpn.heading('name', text='Название сети')
-tree_vpn.heading('login', text='Логин')
-tree_vpn.heading('password', text='Пароль')
-def view_vpn_table():
-    [tree_vpn.delete(i) for i in tree_vpn.get_children()]
-    for row in values_vpn_table():
-        tree_vpn.insert('','end', values=row)
-view_vpn_table()
-vpn_label_frame = LabelFrame(vpn_frame, text='VPN')
-vpn_label_frame.pack(fill="x", padx=20)
-entris_vpn = []
-id_vpn_entry = Entry(vpn_label_frame)
-id_vpn_entry.grid_remove()
-entris_vpn.append(id_vpn_entry)
-owner_vpn_label = Label(vpn_label_frame, text="Владелец")
-owner_vpn_label.grid(row=0, column=0, padx=10, pady=10)
-owner_vpn_entry = Entry(vpn_label_frame)
-owner_vpn_entry.grid(row=0, column=1, padx=10, pady=10)
-entris_vpn.append(owner_vpn_entry)
-name_vpn_label = Label(vpn_label_frame, text="Название сети")
-name_vpn_label.grid(row=0, column=2, padx=10, pady=10)
-name_vpn_entry = Entry(vpn_label_frame)
-name_vpn_entry.grid(row=0, column=3, padx=10, pady=10)
-entris_vpn.append(name_vpn_entry)
-login_vpn_label = Label(vpn_label_frame, text="Логин")
-login_vpn_label.grid(row=0, column=4, padx=10, pady=10)
-login_vpn_entry = Entry(vpn_label_frame)
-login_vpn_entry.grid(row=0, column=5, padx=10, pady=10)
-entris_vpn.append(login_vpn_entry)
-password_vpn_label = Label(vpn_label_frame, text="Пароль")
-password_vpn_label.grid(row=0, column=6, padx=10, pady=10)
-password_vpn_entry = Entry(vpn_label_frame)
-password_vpn_entry.grid(row=0, column=7, padx=10, pady=10)
-entris_vpn.append(password_vpn_entry)
-control_vpn_frame = LabelFrame(vpn_frame, text='Управление списком VPN')
-control_vpn_frame.pack(fill=X, padx=20)
-# Create add vpn func
-def add_vpn():
-    owner = owner_vpn_entry.get()
-    name = name_vpn_entry.get()
-    login = login_vpn_entry.get()
-    password = password_vpn_entry.get()
-    add_new_vpn(owner, name, login, password)
-    for entry in entris_vpn:
-        entry.delete(0, 'end')
-    view_vpn_table()
-# Add button
-add_vpn_button = Button(control_vpn_frame, text="Добавить", cursor='hand2', command=add_vpn)
-add_vpn_button.grid(row=0, column=0, padx=10, pady=10)
-# Button for edit vpn
-def edit_select_vpn():
-    id = id_vpn_entry.get()
-    owner = owner_vpn_entry.get()
-    name = name_vpn_entry.get()
-    login = login_vpn_entry.get()
-    password = password_vpn_entry.get()
-    edit_vpn(id, owner, name, login, password)
-    view_vpn_table()
-edit_vpn_button = Button(control_vpn_frame, text='Изменить', command=edit_select_vpn)
-edit_vpn_button.grid(row=0, column=1, padx=10, pady=10)
-# Button for delete vpn
-def delete_sel_vpn():
-    delete_select_vpn(id_vpn_entry.get())
-    view_vpn_table()
-delete_vpn_button = Button(control_vpn_frame, text="Удалить", cursor='hand2', command=delete_sel_vpn)
-delete_vpn_button.grid(row=0, column=2, padx=10, pady=10)
-
-def delete_all_vpn():
-    result = askyesno(title='Подтверждение операции', message='Вы действительно хотите удалить ВСЕ записи в списке VPN?')
-    if result:
-        remove_all_vpn()
-        view_vpn_table()
-delete_all_vpn_btn = Button(control_vpn_frame, text='Удалить все VPN', cursor='hand2', command=delete_all_vpn)
-delete_all_vpn_btn.grid(row=0, column=3, padx=10, pady=10)
-# Select Record
-def select_record(event):
-    # Clear entry boxes
-    for entry in entris_vpn:
-        entry.delete(0, END)
-    # Grab record Number
-    selected = tree_vpn.focus()
-    # Grab record values
-    values = tree_vpn.item(selected, 'values')
-    # outpts to entry boxes
-    i = 0
-    for entry in entris_vpn:
-        entry.insert(0, values[i])
-        i = i + 1
-tree_vpn.bind('<Double-Button-1>', select_record)
-tree_vpn.yview()
-tree_vpn.pack(side=TOP, fill=X)
 
 tree = ttk.Treeview(tree_frame, yscrollcommand=tree_scroll.set,
                     selectmode="extended",
@@ -277,6 +166,7 @@ button_control_bwd_frame = LabelFrame(bwd_frame, text="Управление па
 button_control_bwd_frame.pack(fill=X, padx=20)
 
 # Create radiobutton for vpn connection------------------------------------------------------------#
+
 vpn_list = []
 vpn_name = select_vpn_name()
 for obj in vpn_name:
@@ -306,11 +196,12 @@ def disable_vpn():
 
 disable_vpn_radiobutton = ttk.Radiobutton(bwd_frame, text='Отключить',variable=state_vpn, value='off', command=disable_vpn)
 disable_vpn_radiobutton.pack(padx=0, pady=0, anchor=NW)
+def show_vpn():
+    for vpn in vpn_list:
+        vpn = ttk.Radiobutton(bwd_frame, text=vpn, variable=state_vpn, value=vpn, command=select_vpn)
+        vpn.pack(padx=0, pady=0, anchor=NW)
+show_vpn()
 
-for vpn in vpn_list:
-    vpn = ttk.Radiobutton(bwd_frame, text=vpn,variable=state_vpn, value=vpn,command=select_vpn)
-    vpn.pack(padx=0,pady=0, anchor=NW)
-    i=i+1
 
 #--------------------------------------------------------------------------------------------------#
 ####################################################################################################
@@ -331,6 +222,119 @@ def select_record(event):
 tree.bind('<Double-Button-1>', select_record)
 #--------------------------------------------------------------------------------------------------#
 
+
+# Create frame for vpn
+
+vpn_list_frame = Frame(vpn_frame)
+vpn_list_frame.pack(pady=10)
+tree_vpn_scroll = Scrollbar(vpn_list_frame)
+tree_vpn_scroll.pack(side=RIGHT, fill=Y)
+tree_vpn = ttk.Treeview(vpn_list_frame, yscrollcommand = tree_vpn_scroll,
+                        selectmode='extended',
+                        columns=('ID','owner','name','login','password'),
+                        height=10, show='headings')
+tree_vpn.column('ID', width=30, anchor=CENTER)
+tree_vpn.column('owner', width=250, anchor=CENTER)
+tree_vpn.column('name',width=250, anchor=CENTER)
+tree_vpn.column('login', width=250,anchor=CENTER)
+tree_vpn.column('password',width=250, anchor=CENTER)
+tree_vpn.heading('ID', text='ID')
+tree_vpn.heading('owner', text='Владелец')
+tree_vpn.heading('name', text='Название сети')
+tree_vpn.heading('login', text='Логин')
+tree_vpn.heading('password', text='Пароль')
+def view_vpn_table():
+    [tree_vpn.delete(i) for i in tree_vpn.get_children()]
+    for row in values_vpn_table():
+        tree_vpn.insert('','end', values=row)
+view_vpn_table()
+vpn_label_frame = LabelFrame(vpn_frame, text='VPN')
+vpn_label_frame.pack(fill="x", padx=20)
+entris_vpn = []
+id_vpn_entry = Entry(vpn_label_frame)
+id_vpn_entry.grid_remove()
+entris_vpn.append(id_vpn_entry)
+owner_vpn_label = Label(vpn_label_frame, text="Владелец")
+owner_vpn_label.grid(row=0, column=0, padx=10, pady=10)
+owner_vpn_entry = Entry(vpn_label_frame)
+owner_vpn_entry.grid(row=0, column=1, padx=10, pady=10)
+entris_vpn.append(owner_vpn_entry)
+name_vpn_label = Label(vpn_label_frame, text="Название сети")
+name_vpn_label.grid(row=0, column=2, padx=10, pady=10)
+name_vpn_entry = Entry(vpn_label_frame)
+name_vpn_entry.grid(row=0, column=3, padx=10, pady=10)
+entris_vpn.append(name_vpn_entry)
+login_vpn_label = Label(vpn_label_frame, text="Логин")
+login_vpn_label.grid(row=0, column=4, padx=10, pady=10)
+login_vpn_entry = Entry(vpn_label_frame)
+login_vpn_entry.grid(row=0, column=5, padx=10, pady=10)
+entris_vpn.append(login_vpn_entry)
+password_vpn_label = Label(vpn_label_frame, text="Пароль")
+password_vpn_label.grid(row=0, column=6, padx=10, pady=10)
+password_vpn_entry = Entry(vpn_label_frame)
+password_vpn_entry.grid(row=0, column=7, padx=10, pady=10)
+entris_vpn.append(password_vpn_entry)
+control_vpn_frame = LabelFrame(vpn_frame, text='Управление списком VPN')
+control_vpn_frame.pack(fill=X, padx=20)
+# Create add vpn func
+def add_vpn():
+    owner = owner_vpn_entry.get()
+    name = name_vpn_entry.get()
+    login = login_vpn_entry.get()
+    password = password_vpn_entry.get()
+    add_new_vpn(owner, name, login, password)
+    for entry in entris_vpn:
+        entry.delete(0, 'end')
+    view_vpn_table()
+    show_vpn()
+# Add button
+add_vpn_button = Button(control_vpn_frame, text="Добавить", cursor='hand2', command=add_vpn)
+add_vpn_button.grid(row=0, column=0, padx=10, pady=10)
+# Button for edit vpn
+def edit_select_vpn():
+    id = id_vpn_entry.get()
+    owner = owner_vpn_entry.get()
+    name = name_vpn_entry.get()
+    login = login_vpn_entry.get()
+    password = password_vpn_entry.get()
+    edit_vpn(id, owner, name, login, password)
+    view_vpn_table()
+    show_vpn()
+edit_vpn_button = Button(control_vpn_frame, text='Изменить', command=edit_select_vpn)
+edit_vpn_button.grid(row=0, column=1, padx=10, pady=10)
+# Button for delete vpn
+def delete_sel_vpn():
+    delete_select_vpn(id_vpn_entry.get())
+    view_vpn_table()
+    show_vpn()
+delete_vpn_button = Button(control_vpn_frame, text="Удалить", cursor='hand2', command=delete_sel_vpn)
+delete_vpn_button.grid(row=0, column=2, padx=10, pady=10)
+
+def delete_all_vpn():
+    result = askyesno(title='Подтверждение операции', message='Вы действительно хотите удалить ВСЕ записи в списке VPN?')
+    if result:
+        remove_all_vpn()
+        view_vpn_table()
+        show_vpn()
+delete_all_vpn_btn = Button(control_vpn_frame, text='Удалить все VPN', cursor='hand2', command=delete_all_vpn)
+delete_all_vpn_btn.grid(row=0, column=3, padx=10, pady=10)
+# Select Record
+def select_record(event):
+    # Clear entry boxes
+    for entry in entris_vpn:
+        entry.delete(0, END)
+    # Grab record Number
+    selected = tree_vpn.focus()
+    # Grab record values
+    values = tree_vpn.item(selected, 'values')
+    # outpts to entry boxes
+    i = 0
+    for entry in entris_vpn:
+        entry.insert(0, values[i])
+        i = i + 1
+tree_vpn.bind('<Double-Button-1>', select_record)
+tree_vpn.yview()
+tree_vpn.pack(side=TOP, fill=X)
 
 
 main_window = mainloop()
